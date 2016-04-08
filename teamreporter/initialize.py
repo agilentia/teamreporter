2d68ac47c03cb6c5698dcfeb14915c00945f1e2d
Teesy <tonyl7126@gmail.com>
2016-04-07 22:19:55 -0700

    remove unused file

--- teamreporter/initialize.py
+++
@@ -1,7 +0,0 @@
-from .models import *
-
-admin = User.objects.create_user(email = "tonyl7126@gmail.com", username = "tonyl7126@gmail.com", password = "test_password")
-team = Team.objects.create(admin = admin, name = "team_name")
-for x in range(7):
-	team.users.add(User.objects.create(email = "user%d@gmail.com" % x, username = "user%d@gmail.com" % x))
-
