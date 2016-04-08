16aec36ec830767eda1062718f3d4c85e43673d7
Daniel Borzęcki <borzecki.daniel@gmail.com>
2016-04-08 18:58:41 +0200

    statics

--- teamreporterapp/settings.py
+++ teamreporterapp/settings.py
@@ -134,7 +134,7 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(PROJECT_ROOT, '../static'),
 ]
 
 # Simplified static file serving.
