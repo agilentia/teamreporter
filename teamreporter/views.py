0dba71fda60bc67ecfdfc0b62fedab7446bcc4d9
Daniel Borzęcki <borzecki.daniel@gmail.com>
2016-04-14 18:00:02 +0200

    prepared generic error page / hooked it up with custom 404 and 500 pages

--- teamreporter/views.py
+++ teamreporter/views.py
@@ -187,6 +187,18 @@ class SurveyView(FormView):
         return super(SurveyView, self).form_valid(form)
 
 
+class GenericErrorPage(TemplateView):
+    template_name = "errors/generic.html"
+    code = None
+    description = None
+
+    def get_context_data(self, **kwargs):
+        context = super(GenericErrorPage, self).get_context_data(**kwargs)
+        context['code'] = self.code
+        context['description'] = self.description
+        return context
+
+
 class ThankYouView(TemplateView):
     template_name = 'thankyou.html'
 
