7492d246d61acb66e48b555c129cf835e0cdf6c5
Daniel Borzęcki <borzecki.daniel@gmail.com>
2016-04-14 19:24:33 +0200

    fix NaN issue > hstep and mstep missing

--- static/javascripts/common/add_modal_controller.js
+++ static/javascripts/common/add_modal_controller.js
@@ -1,27 +1,27 @@
-app.controller('addModalController', ["$scope", "$uibModalInstance", "fields", "title", function($scope, $uibModalInstance, fields, title) {
-	var cur_date = new Date();
+app.controller('addModalController', ["$scope", "$uibModalInstance", "fields", "title", function ($scope, $uibModalInstance, fields, title) {
+    var cur_date = new Date();
+    $scope.data = {};
 
-  for (var i = 0; i < fields.length; i++) {
-    if (fields[i].type == "timepicker") {
-      fields[i].value = cur_date;
+    for (var i = 0; i < fields.length; i++) {
+        if (fields[i].type == "timepicker") {
+            fields[i].value = cur_date;
+            $scope.data[fields[i].var_name] = cur_date;
+        }
     }
-  }
+    $scope.hstep = 1;
+    $scope.mstep = 5;
 
-  $scope.fields = fields;
-	$scope.title = title;
-	$scope.cancel = function () {
-		$uibModalInstance.dismiss('cancel');
-	};
+    $scope.fields = fields;
+    $scope.title = title;
+    $scope.cancel = function () {
+        $uibModalInstance.dismiss('cancel');
+    };
 
-  $scope.time_change = function (val){
-    console.log(val);
-  }
-
-  	$scope.ok = function () {
-  		var result = {};
-  		for (var i = 0; i < $scope.fields.length; i++) {
-  			result[$scope.fields[i].var_name] = $scope.fields[i].value;
-  		}
-    	$uibModalInstance.close(result);
-  	};
+    $scope.ok = function () {
+        var result = {};
+        for (var i = 0; i < $scope.fields.length; i++) {
+            result[$scope.fields[i].var_name] = $scope.fields[i].value;
+        }
+        $uibModalInstance.close(result);
+    };
 }]);
