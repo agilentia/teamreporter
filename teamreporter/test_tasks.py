5ff74dd5e5be855931e6d85f14f49854a459db63
Daniel Borzęcki <borzecki.daniel@gmail.com>
2016-04-17 20:36:21 +0200

    polishing up tests / included summary sending

--- teamreporter/test_tasks.py
+++ teamreporter/test_tasks.py
@@ -1,12 +1,13 @@
 from django.core import mail
 from django.test.utils import override_settings
 from django.test import TestCase
+from django.utils.timezone import now
 
-from .tasks import generate_survey, issue_surveys
+from .tasks import generate_survey, issue_surveys, issue_summaries
 from .models import Report, Team, Membership, User, Role, Survey
 
 from datetime import time
-from unittest.mock import MagicMock
+from unittest import mock
 
 import recurrence
 
@@ -27,10 +28,8 @@ class TestTasks(TestCase):
 
         self.team.membership_set.add(self.membership)
         self.report = Report.objects.create(team=self.team, recurrences=rec, survey_send_time=time.min)
-        self.report.can_issue_daily = MagicMock(return_value=True)
 
-    @override_settings(CELERY_EAGER_PROPAGATES_EXCEPTIONS=True,
-                       CELERY_ALWAYS_EAGER=True,
+    @override_settings(CELERY_ALWAYS_EAGER=True,
                        BROKER_BACKEND='memory')
     def test_generate_survey(self):
         """
@@ -46,8 +45,7 @@ class TestTasks(TestCase):
                                         self.report.get_daily().id)).get()
         self.assertFalse(result, "User is not on this team therefore survey shouldn't be created")
 
-    @override_settings(CELERY_EAGER_PROPAGATES_EXCEPTIONS=True,
-                       CELERY_ALWAYS_EAGER=True,
+    @override_settings(CELERY_ALWAYS_EAGER=True,
                        BROKER_BACKEND='memory')
     def test_issue_survey(self):
         """
@@ -58,16 +56,15 @@ class TestTasks(TestCase):
         """
         result = issue_surveys.apply().get()
         self.assertEqual(result, 1, 'task should generate at least one survey')
-        self.assertEquals(len(mail.outbox), 1)
+        self.assertEquals(len(mail.outbox), 1, 'there should be nice email sent to user')
+
         message = mail.outbox[0]
         html_body = message.alternatives[0][0]
-
         self.assertIn('Boomerang calling in', message.subject, 'title contains positive message')
         self.assertIn(str(Survey.objects.get().pk), message.body, 'body  contains link with survey pk')
         self.assertIn(str(Survey.objects.get().pk), html_body, 'also html body')
 
-    @override_settings(CELERY_EAGER_PROPAGATES_EXCEPTIONS=True,
-                       CELERY_ALWAYS_EAGER=True,
+    @override_settings(CELERY_ALWAYS_EAGER=True,
                        BROKER_BACKEND='memory')
     def test_issue_survey_not_contributor(self):
         self.membership.roles.filter(name='contributor').delete()
@@ -75,11 +72,26 @@ class TestTasks(TestCase):
         result = issue_surveys.apply().get()
         self.assertEqual(result, 0, 'should be sent to contributors only only!!')
 
-    def test_send_survey(self):
-        pass
+    @override_settings(CELERY_ALWAYS_EAGER=True,
+                       BROKER_BACKEND='memory')
+    def test_invalid_issue_survey_conditions(self):
+        with mock.patch.object(Report, 'can_issue_daily', return_value=False) as cid_mock:
+            result = issue_surveys.apply().get()
+            cid_mock.assert_called_with()
 
-    def test_send_summary(self):
-        pass
+        self.assertEqual(result, 0, "can't issue survey without can_issue_daily permission")
 
+    @override_settings(CELERY_ALWAYS_EAGER=True,
+                       BROKER_BACKEND='memory')
     def test_issue_summaries(self):
-        pass
+        result = issue_surveys.apply().get()
+        self.assertEqual(result, 1, 'task should generate at least one survey')
+        survey = Survey.objects.get()
+        survey.completed = now()
+
+        issue_summaries.apply()
+        self.assertEquals(len(mail.outbox), 2, 'there should be two emails sent to user')
+
+        self.assertIsNotNone(Report.objects.get().dailyreport_set.get().summary_submitted)
+        message = mail.outbox[1]
+        self.assertIn('Boomerang summary report', message.subject, 'title contains positive message')
