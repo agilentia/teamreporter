37df2536af1a4fd2a8a87669aeebadf05f645aa6
Teesy <tonyl7126@gmail.com>
2016-04-19 12:29:25 -0700

    add more validation for saves and updates

--- teamreporter/validators.py
+++ teamreporter/validators.py
@@ -7,21 +7,21 @@ def validate_iso_datetime(field, value, error):
         error(field, "invalid iso format")
 
 team_schema = {
-    "name": {"type": "string", "minlength": 1, 'required': True},
-    "days_of_week": {"type": "list", "schema": {'type': 'integer'}, 'required': True},
+    "name": {"type": "string", "minlength": 1, 'maxlength': 30, 'required': True},
+    "days_of_week": {"type": "list", 'minlength': 1, "schema": {'type': 'integer'}, 'required': True},
     "send_time": {"validator": validate_iso_datetime, 'required': True},
     "summary_time": {"validator": validate_iso_datetime, 'required': True},
 }
 
 team_update_schema = {
-    "days_of_week": {"type": "list", "schema": {'type': 'integer'}},
+    "days_of_week": {"type": "list", 'minlength': 1, "schema": {'type': 'integer'}, 'allowed': list(range(7))},
     "send_time": {"validator": validate_iso_datetime},
     "summary_time": {"validator": validate_iso_datetime},
 }
 
 user_schema = {
-    "first_name": {"type": "string", "minlength": 1, 'required': True},
-    "last_name": {"type": "string", "minlength": 1, 'required': True},
+    "first_name": {"type": "string", "minlength": 1, 'maxlength': 30, 'required': True},
+    "last_name": {"type": "string", "minlength": 1, 'maxlength': 30, 'required': True},
     'email': {'type': 'string', 'regex': '^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$', 'required': True},
     'roles': {'type': 'list', 'required': True, 'schema': {'type':'dict', 
                     'schema': {'id': {'type': ['integer', 'string'], 'required': True}, 'name': {'type': 'string', 'required': True} } } }
